28463321c66b9daea4ff9243807f362bbd6b1e11
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-27 09:24:47 +0530

    Refactor using class method

---
+++ EmployeeWagesComputationProgram/EmployeeWagesComputationProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeWagesComputationProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UC7Refactor obj = new UC7Refactor();
+            obj.computeEmpWage();
+
+        }
+            
+    }
+}
